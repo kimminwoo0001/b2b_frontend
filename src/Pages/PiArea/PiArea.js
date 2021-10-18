462a56dfb21c4b2d359a87690a456c837f8ba148
elenasepark <elena_park@teamsnowball.com>
2021-10-18 19:36:21 +0900

    add: mock data로 검색기능 1차 구현

--- src/Pages/PiArea/PiArea.js
+++ src/Pages/PiArea/PiArea.js
@@ -2,25 +2,65 @@ import React, { useState } from 'react'
 import styled, { css } from 'styled-components';
 import SetWardData from './SetWardData/SetWardData';
 import SetPiData from './SetPiData/SetPiData';
+import axios from 'axios';
+import { API3 } from '../config';
 
 function PiArea() {
     const [tab, setTab] = useState(0);
     const [gameIdInput, setGameIdInput] = useState('');
+    const [gameIdSearchData, setGameIdSearchData] = useState([]);
+
     const tabContents = {
         0: <SetWardData />,
         1: <SetPiData />
     }
 
-    const handleSearch = (e) => {
+    // onchange 
+    const handleInput = (e) => {
         setGameIdInput(e.target.value)
-        console.log(gameIdInput);
     }
+
+
+    //onkeyup 
+    const handleSearch = async () => {
+        // const response = await axios.request({
+        //     method: "GET",
+        //     url: `${API3}/gameidsearchAjax.do`,
+        //     url: "http://localhost:3001/data/mockdata.json",
+        //         params: {
+        //             q: gameIdInput,
+        //         },
+        // })
+
+        // CORS 에러 -> 민규님 확인 요청
+        // setGameIdSearchData(response.data.items)
+
+        if (gameIdInput.length < 3) {
+            return;
+        }
+
+        const response = await axios.request({
+            method: 'GET',
+            url: `http://localhost:3001/data/mockdata.json`,
+        })
+        setGameIdSearchData(response.data.items);
+        setGameIdInput('');
+
+    }
+
 
     return (
         <PiAreaWrapper>
             <InnerWrapper>
                 <Title>TeamSnowBall PI Area</Title>
-                <GameIdInput type="text" value={gameIdInput} placeholder="GAME ID 검색" onChange={(e) => handleSearch(e)} />
+                <GameIdInput type="text" value={gameIdInput} placeholder="GAME ID 검색 : 3글자 이상 입력해주세요." onKeyUp={handleSearch} onChange={(e) => handleInput(e)} />
+                <GameIdSearchList isOpen={gameIdSearchData.length > 1}>{
+                    gameIdSearchData?.map((el) => {
+                        return (
+                            <SearchList key={el.gameid}>{el.gameid}</SearchList>
+                        )
+                    })
+                }</GameIdSearchList>
                 <TabWrapper>
                     <WardTab onClick={() => setTab(0)} changeColor={tab === 0}>WARD</WardTab>
                     <PiTab onClick={() => setTab(1)} changeColor={tab === 1}>PI</PiTab>
@@ -28,7 +68,7 @@ function PiArea() {
                 <YoutubeURL type="text" placeholder="Youtube URL을 입력하세요. 공유 -> 시작 시간 : 꼭 체크해주세요!" />
                 <TabContents>{tabContents[tab]}</TabContents>
             </InnerWrapper>
-        </PiAreaWrapper >
+        </PiAreaWrapper>
     )
 }
 
@@ -59,8 +99,21 @@ const GameIdInput = styled.input`
     background-color: #fff;
     border-radius: 5px;
     padding: 10px;
-
 `;
+
+const GameIdSearchList = styled.ul`
+background-color: #fff;
+display: ${(props) => props.isOpen ? "block" : "none"};
+border-radius: 10px;
+padding: 10px;
+border-top: 1px solid black;
+`;
+
+const SearchList = styled.li`
+margin: 5px 0;
+cursor: pointer;
+`;
+
 
 const TabWrapper = styled.div`
     margin:5px 0;
