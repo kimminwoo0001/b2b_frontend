d63930f6be45ede7b769c2a2f11ea1204a690e8b
elenasepark <elena_park@teamsnowball.com>
2021-11-11 14:53:33 +0900

    filter 전체선택 오류 수정

--- src/Pages/Solo/PlayerBoard/PlayerBoard.js
+++ src/Pages/Solo/PlayerBoard/PlayerBoard.js
@@ -21,7 +21,6 @@ import {
 } from "../../../redux/modules/filtervalue";
 import axiosRequest from "../../../lib/axiosRequest";
 
-
 function PlayerBoard() {
   //선수 보고서 => 선수 상황판
   const filters = useSelector((state) => state.FilterReducer);
@@ -117,7 +116,7 @@ function PlayerBoard() {
       });
       setSeason({ x: seasonX, y: seasonY });
       setLoading(false);
-    })
+    });
   };
 
   //챔피언 필터
@@ -135,7 +134,7 @@ function PlayerBoard() {
     axiosRequest(url, params, function (e) {
       setChampFilter(e.data.champion);
       setChampEng(e.data.championEng);
-    })
+    });
   };
 
   //상대 챔피언 필터
@@ -151,11 +150,11 @@ function PlayerBoard() {
       player: filters.player,
       token: user.token,
       id: user.id,
-    }
+    };
     axiosRequest(url, params, function (e) {
       setOppFilter(e.data.champion);
       setOppEng(e.data.championEng);
-    })
+    });
   };
 
   // 그래프 세팅 값
@@ -232,8 +231,9 @@ function PlayerBoard() {
             <div className="AttendValue">
               <span className="Wins">{`${matchInfo?.match}${t(
                 "solo.playerboard.games"
-              )} ${matchInfo?.win}${t("solo.playerboard.win")} ${matchInfo?.loss
-                }${t("solo.playerboard.lose")}`}</span>
+              )} ${matchInfo?.win}${t("solo.playerboard.win")} ${
+                matchInfo?.loss
+              }${t("solo.playerboard.lose")}`}</span>
               <span className="WinRate">{`${matchInfo?.winrate.toFixed(
                 1
               )}%`}</span>
@@ -244,7 +244,10 @@ function PlayerBoard() {
               {t("solo.playerboard.avgScore")}
             </div>
 
-            <div className="PerformanceValue">{sbr?.sbrAvg.toFixed(1)} / {sbr?.price > 0 ? sbr?.price + "위" : "출전 경기 부족"}</div>
+            <div className="PerformanceValue">
+              {sbr?.sbrAvg.toFixed(1)} /{" "}
+              {sbr?.price > 0 ? sbr?.price + "위" : "출전 경기 부족"}
+            </div>
           </div>
           <div className="AverageBoxTwo">
             <div className="PerformanceTitle">
@@ -354,7 +357,6 @@ function PlayerBoard() {
             <button
               className="Select"
               onClick={() => {
-
                 dispatch(ResetChampion2(""));
                 GetPlayerBoardData();
               }}
@@ -955,6 +957,7 @@ const LeftInfo = styled.div`
 const FilterBox = styled.div`
   display: flex;
   align-items: center;
+  border-radius: 20px;
   .Vs {
     font-family: Poppins;
     font-size: 15px;
@@ -1041,6 +1044,7 @@ const AbilitySection = styled.div`
   min-height: 300px;
   border: solid 1px rgb(58, 55, 69);
   background-color: rgb(47, 45, 56);
+  border-radius: 20px;
 `;
 
 const RecordSection = styled.div`
@@ -1063,10 +1067,12 @@ const PlayerOverView = styled.div`
   width: 100%;
   height: 79px;
   margin: 22.5px 0px 0 0;
+  border-radius: 20px;
   border: solid 1px #3a3745;
   background-color: #2f2d38;
   background-image: url("Images/left-red-gradient.png");
   background-repeat: no-repeat;
+
   .DetailInfo {
     display: flex;
     flex-direction: column;
@@ -1226,6 +1232,8 @@ const AbilityContents = styled.div`
   min-height: 260px;
   border: solid 1px rgb(67, 63, 78);
   background-color: rgb(58, 55, 69);
+  border-bottom-right-radius: 20px;
+  border-bottom-left-radius: 20px;
   padding: 20px;
 `;
 
@@ -1251,7 +1259,8 @@ const StatBox = styled.table`
   height: 100%;
   background-color: rgb(47, 45, 56);
   margin-right: 22px;
-  border: 1px solid rgb(35, 33, 42);
+  border: 1px solid #2f2d38;
+  /* border-radius: 20px; */
   :nth-child(3) {
     margin-right: 0px;
   }
@@ -1644,6 +1653,7 @@ const DropDownContainer = styled.div`
     letter-spacing: -0.6px;
     text-align: left;
     color: rgb(175, 173, 190);
+    border-radius: 10px;
   }
 
   .menu-trigger:hover {
