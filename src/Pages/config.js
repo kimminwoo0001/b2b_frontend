0c91b85fe6e8f497ee24c1ca545b12ac6afc9e17
elenasepark <elena_park@teamsnowball.com>
2021-11-11 17:12:34 +0900

    wip

--- src/Pages/config.js
+++ src/Pages/config.js
@@ -2,7 +2,6 @@
 // export const API = "https://nunu.gg:8443";
 // export const API = "http://nunu.gg:8901"; // 테스트 서버
 export const API = "http://nunu.gg:8787"; // 테스트 서버
-// export const API = "http://nunu.gg:8989"; // 테스트 서버
 
 //API2: 영상보고서
 // export const API2 = "https://nunu.gg:8443";
