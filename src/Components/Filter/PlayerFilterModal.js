586df4b1f22c7baea8ec05421a8e7e08523e923c
MinSeok0901 <kthrkdals@teamsnowball.com>
2022-01-13 23:05:37 +0900

    add

--- src/Components/Filter/PlayerFilterModal.js
+++ src/Components/Filter/PlayerFilterModal.js
@@ -140,7 +140,7 @@ function PlayerFilterModal() {
     ) {
       yearList.push(
         //Object.keys(Object.values(staticvalue.filterObjects)[i])[0]
-        "2021"
+        "2022"
       );
     }
     const recentYear = yearList
