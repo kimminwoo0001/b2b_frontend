5931630d8d88539727f32ee0e83e3468f963bba3
elenasepark <elena_park@teamsnowball.com>
2022-02-03 19:58:18 +0900

    bugfix: 선수비교 능력치 탭 response에 맞춘 수식 수정

--- src/Pages/Solo/ComparePlayer/Stats.js
+++ src/Pages/Solo/ComparePlayer/Stats.js
@@ -105,8 +105,9 @@ function Stats() {
       function (e) {
         setData(e?.player);
         setOppData(e?.oppPlayer);
+
         //라인전 능력치 비교 그래프 데이터 가공
-        const playerFilteredData = Object.values(e?.player.LineStat)?.filter((line) =>
+        const playerFilteredData = e?.player.LineStat2?.filter((line) =>
           line.value !== "NULL"
         )
         const lineData = playerFilteredData.map((line) => {
@@ -118,7 +119,7 @@ function Stats() {
           };
         });
 
-        const oppplayerFilteredData = Object.values(e?.player.LineStat)?.filter((line) =>
+        const oppplayerFilteredData = e?.oppPlayer.LineStat2?.filter((line) =>
           line.value !== "NULL"
         )
         const lineData2 = oppplayerFilteredData.map((line) => {
@@ -135,9 +136,10 @@ function Stats() {
           setLineStat(lineData);
         }
         console.log(Object.values(lineData));
+
         //교전 능력치 비교 그래프 데이터 가공
 
-        const playerMatchData = Object.values(e?.player.MatchStat)?.filter((match) => match.value !== "NULL");
+        const playerMatchData = e?.player.MatchStat2?.filter((match) => match.value !== "NULL");
         const matchData = playerMatchData.map((match) => {
           return {
             x1: match.percent.toFixed(1),
@@ -147,7 +149,7 @@ function Stats() {
           };
         });
 
-        const oppplayerMatchData = Object.values(e?.oppPlayer.MatchStat)?.filter((match) => match.value !== "NULL");
+        const oppplayerMatchData = e?.oppPlayer.MatchStat2?.filter((match) => match.value !== "NULL");
         const matchData2 = oppplayerMatchData.map(
           (match) => {
             return {
