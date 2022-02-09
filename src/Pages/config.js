c8e555187b9392a694dc9930b9294b7e00cb012d
MinSeok0901 <kthrkdals@teamsnowball.com>
2022-02-09 13:59:07 +0900

    add2

--- src/Pages/config.js
+++ src/Pages/config.js
@@ -8,7 +8,8 @@ export const API = "http://nunu.gg:8903"; // 테스트 서버
 
 //API2: 영상보고서
 // export const API2 = "https://nunu.gg:8443";
-export const API2 = "http://nunu.gg:8787"; // 테스트 서버
+//export const API2 = "http://nunu.gg:8787"; // 테스트 서버
+export const API2 = "http://nunu.gg:8903"; // 테스트 서버
 
 //API3: PI DATA
 export const API3 = "https://nunu.gg:8443/LeagueCollectionPi";
