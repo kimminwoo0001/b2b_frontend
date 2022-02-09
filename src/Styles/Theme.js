01c737fa41c5434c26eeeaeff34ba73826ebaae2
kimminwoo <kimminwoo@teamsnowball.com>
2022-02-09 15:03:47 +0900

    add : theme, test style 추가

--- src/Styles/Theme.js
+++ src/Styles/Theme.js
@@ -1,13 +1,21 @@
 const theme = {
   colors: {
+    // maincolor
     point: "#5942ba",
     default: "#484655",
+
+    // bordercolor
+    border_light: "#433f4e",
+
+    // bg
     bg: "#2f2d38",
     bg_gnb: "#16151c",
     bg_light: "#2e2d38",
     bg_box: "#2f2d38",
-    info: "#84818e",
+
+    // text
     text: "#ffffff",
+    info: "#84818e",
     error: "#ee1717",
   },
   borderRadius: "20px",
