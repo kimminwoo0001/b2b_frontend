0c549435f9397bec3810d137eade0dcf3000e9a5
elenasepark <elena_park@teamsnowball.com>
2022-02-10 11:17:36 +0900

    bugfix: 선수보고서 10미만 minute 최초 0 제거

--- src/Pages/Solo/PlayerBoard/PlayerBoard.js
+++ src/Pages/Solo/PlayerBoard/PlayerBoard.js
@@ -58,7 +58,7 @@ function PlayerBoard() {
 
 
   const secToMin = (sec) => {
-    let mm = addZero(Math.floor(sec / 60));
+    let mm = Math.floor(sec / 60);
     let ss = addZero(Math.floor(sec % 60));
 
     return `${mm}${t("solo.playerboard.min")} ${ss}${t("solo.playerboard.sec")}`;
