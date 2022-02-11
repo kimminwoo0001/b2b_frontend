b80af12f9641972776eb040e3b101f47d9da59e8
MinSeok0901 <kthrkdals@teamsnowball.com>
2022-02-11 17:10:15 +0900

    add

--- src/Pages/SoloRank/Component/MyTeam/MTContent.jsx
+++ src/Pages/SoloRank/Component/MyTeam/MTContent.jsx
@@ -1,20 +1,52 @@
 /** @jsxImportSource @emotion/react */
 import { jsx, css } from "@emotion/react";
 import styled from "@emotion/styled/macro";
+import React from "react";
 
 import { useTranslation } from "react-i18next";
+import DropdownItem from "../../../../Components/Ui/DropDown/DropdownItem";
+import DropdownList from "../../../../Components/Ui/DropDown/DropdownList";
+import Sortingimage from "../../../../Components/Ui/Sortingimage";
 import ItemBox from "./SubComponent/ItemBox";
 
+const inquireDayList = [1, 3, 5, 7, 15, 30];
+
+const useSortableData = (tiers, config = null) => {
+  const [sortConfig, setSortConfig] = React.useState(config);
+
+  const sortedItems = React.useMemo(() => {
+    let sortableItems = [...tiers];
+    if (sortConfig !== null) {
+      sortableItems.sort((a, b) => {
+        if (a[sortConfig.key] < b[sortConfig.key]) {
+          return sortConfig.direction === "ascending" ? -1 : 1;
+        }
+        if (a[sortConfig.key] > b[sortConfig.key]) {
+          return sortConfig.direction === "ascending" ? 1 : -1;
+        }
+        return 0;
+      });
+    }
+    return sortableItems;
+  }, [tiers, sortConfig]);
+};
+
 const MTContent = () => {
+  // 챔피언 티어 오름차 내림차  정렬 상태값
+  const tier = [];
+  const { tiers, requestSorts } = useSortableData(tier ? tier : []);
   const { t } = useTranslation();
+
   return (
     <SWrapper>
       <SCategory>
-        <div className="label">
+        <div className="label addInterestedPlayer">
           {t("soloRank.myTeam.label.addInterestedPlayer")}
         </div>
-        <div className="label">{t("soloRank.myTeam.label.soloRankID")}</div>
-        <div className="label">
+        <div className="label soloRankID">
+          {t("soloRank.myTeam.label.soloRankID")}
+        </div>
+        <div className="label curTier8prevTier">
           {t("soloRank.myTeam.label.curTier8prevTier")
             .split("\n")
             .map((line) => {
@@ -25,14 +57,17 @@ const MTContent = () => {
                 </>
               );
             })}
+          <Sortingimage />
         </div>
-        <div className="label">{t("soloRank.myTeam.label.curSeason")}</div>
-        <div className="label">{t("common.label.winrate")}</div>
-        <div className="label">
+        <div className="label curSeason">
+          {t("soloRank.myTeam.label.curSeason")}
+        </div>
+        <div className="label winrate">{t("common.label.winrate")}</div>
+        <div className="label recentDays">
           {t("soloRank.myTeam.label.recentDays").replace("##", "30")}
         </div>
-        <div className="label">{t("common.label.winrate")}</div>
-        <div className="label">
+        <div className="label winrate">{t("common.label.winrate")}</div>
+        <div className="label daysPlayedCham">
           {t("soloRank.myTeam.label.daysPlayedCham")
             .split("\n")
             .map((line) => {
@@ -44,9 +79,20 @@ const MTContent = () => {
               );
             })}
         </div>
-        <button className="label">
-          {t("soloRank.myTeam.label.addInterestedPlayer")}
-        </button>
+        <DropdownList
+          label={t("soloRank.myTeam.label.recentDays").replace("##", "30")}
+          onChange={() => {}}
+        >
+          {inquireDayList.map((day) => {
+            return (
+              <DropdownItem
+                value={t("soloRank.myTeam.label.recentDays").replace("##", day)}
+              >
+                {t("soloRank.myTeam.label.recentDays").replace("##", day)}
+              </DropdownItem>
+            );
+          })}
+        </DropdownList>
       </SCategory>
     </SWrapper>
   );
@@ -66,7 +112,7 @@ const SCategory = styled.div`
   margin: 0 0 5px;
   padding: 0 0 0 14px;
   display: flex;
-  justify-content: space-between;
+  //justify-content: space-between;
 
   .label {
     height: 19px;
@@ -80,30 +126,29 @@ const SCategory = styled.div`
     letter-spacing: normal;
     text-align: left;
     color: #817e8b;
+    display: flex;
+    align-items: center;
   }
 
-  // .addInterestedPlayer {
-  //   width: 153px;
-  // }
-  // .soloRankID {
-  //   width: 167px;
-  // }
-  // .curTier8prevTier {
-  //   width: 230px;
-  // }
-  // .curSeason {
-  //   width: 153px;
-  // }
-  // .winrate {
-  //   width: 153px;
-  // }
-  // .recent {
-  //   width: 153px;
-  // }
-  // .addInterestedPlayer {
-  //   width: 153px;
-  // }
-  // .addInterestedPlayer {
-  //   width: 153px;
-  // }
+  .addInterestedPlayer {
+    width: 153px;
+  }
+  .soloRankID {
+    width: 167px;
+  }
+  .curTier8prevTier {
+    width: 230px;
+  }
+  .curSeason {
+    width: 105px;
+  }
+  .winrate {
+    width: 66px;
+  }
+  .recentDays {
+    width: 93px;
+  }
+  .daysPlayedCham {
+    width: 121px;
+  }
 `;
