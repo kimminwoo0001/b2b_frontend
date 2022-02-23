bacbf27871e3cd5afcd2ca6327437621109797e6
kimminwoo <kimminwoo@teamsnowball.com>
2022-02-23 09:14:25 +0900

    fix : UI - positionCheckbox 다중 / 단일 선택 기능 추가

--- src/Components/Ui/CustomCheckbox.jsx
+++ src/Components/Ui/CustomCheckbox.jsx
@@ -4,7 +4,10 @@ import { cx } from "@emotion/css";
 
 const CustomCheckbox = ({ checked, children, className, ...props }) => {
   return (
-    <SContainer className={cx(className, checked ? "is-active" : "")}>
+    <SContainer
+      tabIndex={0}
+      className={cx(className, checked ? "is-active" : "")}
+    >
       <input type="checkbox" checked={checked} {...props} />
       {children}
     </SContainer>
