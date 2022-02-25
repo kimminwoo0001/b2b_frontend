dc1ad4a9f69c6d452a02b01e76d7b58a7ef03c72
kimminwoo <kimminwoo@teamsnowball.com>
2022-02-26 00:48:51 +0900

    refac : 6. 필터 하위 렌더링 조건 변경 (team, season 선택)

--- src/Pages/PathingAnalysis/JungleMap/screens/Sequence.js
+++ src/Pages/PathingAnalysis/JungleMap/screens/Sequence.js
@@ -1,6 +1,6 @@
 /** @jsxImportSource @emotion/react */
 import { jsx } from "@emotion/react";
-import styled from "@emotion/styled";
+
 import { useSelector } from "react-redux";
 import PositionCheckList from "../../../../Components/Ui/PositionCheckList";
 import JungleFilter from "../components/JungleFilter";
@@ -8,9 +8,13 @@ import JungleSideFilter from "../components/JungleSideFilter";
 import SequenceDialog from "../components/SequenceDialog";
 import Map from "../components/Map";
 import * as S from "../components/styled/StyledJungleLayout";
+import { getTrueValueList } from "../../../../lib/getTureValueList";
 
 const Sequence = () => {
-  const junglevalue = useSelector((state) => state.JungleMapReducer);
+  const patchList = useSelector((state) => state.JungleMapReducer.patch);
+  const teamList = useSelector((state) => state.JungleMapReducer.team);
+  const selectedPatch = getTrueValueList(patchList);
+
   return (
     <S.SequenceContainer>
       {/* 메인 필터 */}
@@ -18,32 +22,29 @@ const Sequence = () => {
         <JungleFilter />
       </S.FilterContainer>
       {/* 사이드 필터와 맵 비디오  */}
-      {Object.keys(junglevalue.patch).length > 0 &&
-        Object.keys(junglevalue.patch).filter(
-          (key) => junglevalue.patch[key] === true
-        ).length > 0 && (
-          <S.FlexContainer>
-            {/* 사이드 필터 */}
-            <S.Sidebar>
-              <JungleSideFilter />
-            </S.Sidebar>
+      {teamList.length > 0 && selectedPatch.length > 0 && (
+        <S.FlexContainer>
+          {/* 사이드 필터 */}
+          <S.Sidebar>
+            <JungleSideFilter />
+          </S.Sidebar>
 
-            {/*  맵 & 다이얼로그 */}
-            <S.Container>
-              {/* 맵 비디오 */}
-              <S.VideoContainer>
-                <PositionCheckList
-                  onChange={(position) => console.log(position)}
-                />
-                <Map />
-              </S.VideoContainer>
-              {/* 이벤트 다이얼로그 */}
-              <S.DialogContainer>
-                <SequenceDialog />
-              </S.DialogContainer>
-            </S.Container>
-          </S.FlexContainer>
-        )}
+          {/*  맵 & 다이얼로그 */}
+          <S.Container>
+            {/* 맵 비디오 */}
+            <S.VideoContainer>
+              <PositionCheckList
+                onChange={(position) => console.log(position)}
+              />
+              <Map />
+            </S.VideoContainer>
+            {/* 이벤트 다이얼로그 */}
+            <S.DialogContainer>
+              <SequenceDialog />
+            </S.DialogContainer>
+          </S.Container>
+        </S.FlexContainer>
+      )}
     </S.SequenceContainer>
   );
 };
