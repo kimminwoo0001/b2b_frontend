dbb845983a64473c9fd715727ebbc4c981b0bed2
kimminwoo <kimminwoo@teamsnowball.com>
2022-03-07 12:32:47 +0900

    add : UI - NotFound  에러페이지 추가

---
+++ src/Components/Ui/Error/NotFound.jsx
@@ -0,0 +1,29 @@
+import styled from "@emotion/styled";
+import React from "react";
+import { spacing, typoStyle } from "../../../Styles/ui";
+
+const NotFound = ({ text, props }) => {
+  return (
+    <Container {...props}>
+      <ImgContainer>
+        <img src="images/icon/icon_error_page.svg" alt="NotFound" />
+      </ImgContainer>
+      <TextContainer>{text}</TextContainer>
+    </Container>
+  );
+};
+
+const Container = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+`;
+const ImgContainer = styled.div`
+  ${spacing.marginB(4)}
+`;
+const TextContainer = styled.div`
+  ${typoStyle.contents_title}
+`;
+
+export default NotFound;
